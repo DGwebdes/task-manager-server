9b5c1e2dd188cc7a9140d74e57078bb1ef8a3338
DGwebdes <alan.bilik8@gmail.com>
2025-03-13 02:59:23 +0000

    updating dependencies

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const connectDB = require('./db/db')
-const taskRouter = require('./routes/taskRoutes');
-const authRouter = require('./routes/authRoutes');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const connectDB = require("./db/db");
+const taskRouter = require("./routes/taskRoutes");
+const authRouter = require("./routes/authRoutes");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 // Trust the first proxy
-app.set('trust proxy', 1);
+app.set("trust proxy", 1);
 
 app.use(cors());
 app.use(express.json());
 //Connecting to DB
 connectDB();
 //Routers
-app.use('/tasks', taskRouter);
-app.use('/auth', authRouter);
+app.use("/tasks", taskRouter);
+app.use("/auth", authRouter);
 
-
-//initilize server
+//initialize server
 app.listen(PORT, () => {
     console.log(`App listening on port :${PORT}`);
-})
+});
